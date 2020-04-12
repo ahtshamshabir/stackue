24c6f11700ad8e269b822182c3253d27a5b7e82c
Ahtsham Shabir <ahtshamshabir@yahoo.com>
2020-04-12 22:39:19 +0500

    added vote control and m2m polymorphic

--- resources/views/questions/show.blade.php
+++ resources/views/questions/show.blade.php
@@ -10,27 +10,42 @@
 							<div class="d-flex align-items-center">
 								<h1>{{$question->title}}</h1>
 								<div class="ml-auto">
-									<a class="btn btn-outline-secondary" href="{{route('questions.index')}}">Back to all Questions</a>
+									<a class="btn btn-outline-secondary" href="{{route('questions.index')}}">Back to all
+										Questions</a>
 								</div>
 							</div>
 						</div>
 						<hr>
 						<div class="media">
 							<div class="d-flex flex-column vote-controls">
-								<a title="This question is useful" class="vote-up" href="#">
+								<a title="This question is useful" class="vote-up {{Auth::guest() ? 'off' : ''}}"
+								   onclick="event.preventDefault();document.getElementById('up-vote-question-{{$question->id}}').submit();" href="#">
 									<i class="fas fa-caret-up fa-3x"></i>
 								</a>
-								<span class="votes-count">123</span>
-								<a title="This question is not useful" class="vote-down off" href="#">
+								<form id="up-vote-question-{{$question->id}}"
+									  action="/questions/{{$question->id}}/vote" method="post" hidden>
+									@csrf
+									<input type="hidden" name="vote" value="1">
+								</form>
+								<span class="votes-count">{{$question->votes_count}}</span>
+								<a title="This question is not useful" class="vote-down {{Auth::guest() ? 'off' : ''}}"
+								   onclick="event.preventDefault();document.getElementById('down-vote-question-{{$question->id}}').submit();"href="#">
 									<i class="fas fa-caret-down fa-3x"></i>
 								</a>
+								<form id="down-vote-question-{{$question->id}}"
+									  action="/questions/{{$question->id}}/vote" method="post" hidden>
+									@csrf
+									<input type="hidden" name="vote" value="-1">
+								</form>
 								<a title="Click to mark as favorite question (Click again to undo)"
-								   class="{{Auth::guest()? 'off' : ($question->is_favorited ? 'favorited' : '')}} favorite mt-2" href="#"
+								   class="{{Auth::guest() ? 'off' : ($question->is_favorited ? 'favorited' : '')}} favorite mt-2"
+								   href="#"
 								   onclick="event.preventDefault();document.getElementById('favorite-question-{{$question->id}}').submit();">
 									<i class="fas fa-star fa-2x"></i>
 									<span class="favorites-count">{{$question->favorites_count}}</span>
 								</a>
-								<form id="favorite-question-{{$question->id}}" action="/questions/{{$question->id}}/favorites" method="post" hidden>
+								<form id="favorite-question-{{$question->id}}"
+									  action="/questions/{{$question->id}}/favorites" method="post" hidden>
 									@csrf
 									@if($question->is_favorited)
 										@method ('DELETE')
@@ -42,7 +57,8 @@
 								<div class="float-right">
 									<span class="text-muted">Asked {{$question->created_at}}</span>
 									<div class="media mt-2">
-										<a href="{{$question->user->url}}" class="pr-2"><img src="{{$question->user->avatar}}" alt="author image"></a>
+										<a href="{{$question->user->url}}" class="pr-2"><img
+													src="{{$question->user->avatar}}" alt="author image"></a>
 										<div class="media-body mt-1">
 											<a href="{{$question->user->url}}">{{$question->user->name}}</a>
 										</div>
