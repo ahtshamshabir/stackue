711c29dc06a1477f9347aa3f5d858b1eb5f22a4e
Ahtsham Shabir <ahtshamshabir@yahoo.com>
2020-04-17 16:27:22 +0500

    inline question template vue js

--- resources/views/answers/_answer.blade.php
+++ resources/views/answers/_answer.blade.php
@@ -1,34 +1,44 @@
-<div class="media post">
-	@include('shared._vote',['model' => $answer])
-	<div class="media-body">
-		{!! $answer->body_html !!}
-		<div class="row">
-			<div class="col-4">
-				<div class="ml-auto">
-					@can('update', $answer)
-						<a class="btn btn-sm btn-outline-info"
-						   href="{{route('questions.answers.edit', [$question->id, $answer->id])}}">Edit</a>
-					@endcan
-					@can('delete', $answer)
-						<form class="form-delete"
-							  action="{{route('questions.answers.destroy', [$question->id, $answer->id])}}"
-							  method="post">
-							{{--												{{method_field('DELETE')}}--}}
-							@method('DELETE')
-							@csrf
-							<button type="submit" class="btn btn-sm btn-outline-danger"
-									onclick="return confirm('Are you sure?')">Delete
-							</button>
-						</form>
-					@endcan
+<answer :answer="{{$answer}}" inline-template>
+	<div class="media post">
+		@include('shared._vote',['model' => $answer])
+		<div class="media-body">
+			<form id="form" v-if="editing" action="#" @submit.prevent="update">
+				<div class="form-group">
+					<textarea rows="10" v-model="body" class="form-control" required></textarea>
 				</div>
-			</div>
-			<div class="col-4">
+				<Button class="btn btn-primary" type="submit" :disabled="isInvalid">Update</Button>
+				<Button class="btn btn-outline-secondary" @click="cancel" type="button">Cancel</Button>
+			</form>
+			<div v-else>
+				<div v-html="bodyHtml"></div>
+				<div class="row">
+					<div class="col-4">
+						<div class="ml-auto">
+							@can('update', $answer)
+								<a @click.prevent="edit" class="btn btn-sm btn-outline-info">Edit</a>
+							@endcan
+							@can('delete', $answer)
+								<form class="form-delete"
+									  action="{{route('questions.answers.destroy', [$question->id, $answer->id])}}"
+									  method="post">
+									{{--												{{method_field('DELETE')}}--}}
+									@method('DELETE')
+									@csrf
+									<button type="submit" class="btn btn-sm btn-outline-danger"
+											onclick="return confirm('Are you sure?')">Delete
+									</button>
+								</form>
+							@endcan
+						</div>
+					</div>
+					<div class="col-4">
 
-			</div>
-			<div class="col-4">
-				<user-info :model="{{$answer}}" label="Answered"></user-info>
+					</div>
+					<div class="col-4">
+						<user-info :model="{{$answer}}" label="Answered"></user-info>
+					</div>
+				</div>
 			</div>
 		</div>
 	</div>
-</div>
+</answer>
--- storage/framework/views/7b094ec9e19ad2b851be1116c80200a76f4e41a3.php
+++ storage/framework/views/7b094ec9e19ad2b851be1116c80200a76f4e41a3.php
@@ -1,35 +1,44 @@
-<div class="media post">
-	<?php echo $__env->make('shared._vote',['model' => $answer], array_except(get_defined_vars(), array('__data', '__path')))->render(); ?>
-	<div class="media-body">
-		<?php echo $answer->body_html; ?>
-
-		<div class="row">
-			<div class="col-4">
-				<div class="ml-auto">
-					<?php if (app(\Illuminate\Contracts\Auth\Access\Gate::class)->check('update', $answer)): ?>
-						<a class="btn btn-sm btn-outline-info"
-						   href="<?php echo e(route('questions.answers.edit', [$question->id, $answer->id])); ?>">Edit</a>
-					<?php endif; ?>
-					<?php if (app(\Illuminate\Contracts\Auth\Access\Gate::class)->check('delete', $answer)): ?>
-						<form class="form-delete"
-							  action="<?php echo e(route('questions.answers.destroy', [$question->id, $answer->id])); ?>"
-							  method="post">
-							
-							<?php echo method_field('DELETE'); ?>
-							<?php echo csrf_field(); ?>
-							<button type="submit" class="btn btn-sm btn-outline-danger"
-									onclick="return confirm('Are you sure?')">Delete
-							</button>
-						</form>
-					<?php endif; ?>
+<answer :answer="<?php echo e($answer); ?>" inline-template>
+	<div class="media post">
+		<?php echo $__env->make('shared._vote',['model' => $answer], array_except(get_defined_vars(), array('__data', '__path')))->render(); ?>
+		<div class="media-body">
+			<form id="form" v-if="editing" action="#" @submit.prevent="update">
+				<div class="form-group">
+					<textarea rows="10" v-model="body" class="form-control" required></textarea>
 				</div>
-			</div>
-			<div class="col-4">
+				<Button class="btn btn-primary" type="submit" :disabled="isInvalid">Update</Button>
+				<Button class="btn btn-outline-secondary" @click="cancel" type="button">Cancel</Button>
+			</form>
+			<div v-else>
+				<div v-html="bodyHtml"></div>
+				<div class="row">
+					<div class="col-4">
+						<div class="ml-auto">
+							<?php if (app(\Illuminate\Contracts\Auth\Access\Gate::class)->check('update', $answer)): ?>
+								<a @click.prevent="edit" class="btn btn-sm btn-outline-info">Edit</a>
+							<?php endif; ?>
+							<?php if (app(\Illuminate\Contracts\Auth\Access\Gate::class)->check('delete', $answer)): ?>
+								<form class="form-delete"
+									  action="<?php echo e(route('questions.answers.destroy', [$question->id, $answer->id])); ?>"
+									  method="post">
+									
+									<?php echo method_field('DELETE'); ?>
+									<?php echo csrf_field(); ?>
+									<button type="submit" class="btn btn-sm btn-outline-danger"
+											onclick="return confirm('Are you sure?')">Delete
+									</button>
+								</form>
+							<?php endif; ?>
+						</div>
+					</div>
+					<div class="col-4">
 
-			</div>
-			<div class="col-4">
-				<user-info :model="<?php echo e($answer); ?>" label="Answered"></user-info>
+					</div>
+					<div class="col-4">
+						<user-info :model="<?php echo e($answer); ?>" label="Answered"></user-info>
+					</div>
+				</div>
 			</div>
 		</div>
 	</div>
-</div>
+</answer>
